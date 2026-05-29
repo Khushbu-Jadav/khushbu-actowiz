5421db3d6654e59667023a667fcab2173885fd8d
Khushbu-Jadav <khushbujadavactowiz@gmail.com>
2026-05-29 17:53:34 +0530

    updated dell scraping

--- dell_scrap/dell_db.py
+++ dell_scrap/dell_db.py
@@ -7,7 +7,8 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 my_db = myclient["dell_database"]
 
 collection = my_db["laptops"]
-collection.create_index("order_code", unique=True)
+collection.delete_many({})
+# collection.create_index("order_code", unique=True)
 
 with open(r"C:\python practice\dell_scrap\dell_scraped_data.json","r",encoding="utf-8") as f:
     data = json.load(f)
